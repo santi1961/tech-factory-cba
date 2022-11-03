a154fb449dff0ac54d2f8fb482fbb514931faea0
santi1961 <santiconrero@hotmail.com>
2022-11-03 19:49:37 -0300

    Add js

---
+++ app.js
@@ -0,0 +1,8 @@
+let perfilComprador = prompt("Le recordamos que solo hacemos ventas mayoristas esta de acuerdo? (S/N)");
+
+if (perfilComprador = "S") {
+    alert = ("Puede proceder a comprar");
+} else {
+    alert = ("Lo sentimos, solo realizamos ventas mayoristas");
+}
+
